fdbc3de375e59d1b23dde138bdb57e92f650d19e
tarik <tarikozgur0607@gmail.com>
2022-07-23 15:34:43 +0300

    add progress

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
   // htmldeki divi oluştur
+ //row
+ //rowun özelliklerini ayarla
+ let row = document.createElement("div");
+ //container
+ const container = document.querySelector("#container")
  
-  //256 kere çalışacak bir div oluştur
-  for(let i = 0; i<7; i++){
+ function row_squares(){
+    for (let i = 0; i <16; i++) {
+    let childRow = document.createElement("div")
+    childRow = document.createElement("div");
+    childRow.style.width = "10px";
+    childRow.style.height = "10px";
+    childRow.style.border = "3px solid red"
+    childRow.style.display = "flex";
+    childRow.style.flexDirection = "row";
+    container.appendChild(row);
+    row.appendChild(childRow);
+}
+
+ 
+}
+row_squares();
+ //cells
+const cells = document.createElement("div");
+ //256 kere çalışacak bir div oluştur
+  for(let i = 0; i<16; i++){
       let div = document.createElement("div");
-      div.textContent = "oluştum amk";
-      document.body.appendChild(div);
+      
+      div.style.width = "10px";
+      div.style.height = "10px";
+      div.style.border = "3px solid red";
+      
+      document.getElementById("container").appendChild(div);
     }
    
 
