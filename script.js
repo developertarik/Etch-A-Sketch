66ef5c4636b44cc0f6848db80259a50b7a357db2
tarik <tarikozgur0607@gmail.com>
2022-07-23 21:53:07 +0300

    add 16x16 grid

--- script.js
+++ script.js
@@ -6,48 +6,42 @@
  const container = document.querySelector("#container")
  container.style.display = "flex";
  container.style.flexDirection = "column";
+ container.style.width = "600px";
+ container.style.height = "380px";
+ container.style.flexWrap = "wrap";
+ container.style.boxSizing = "border-box";
 
  //container
  
  function row_squares(){
+  
     for (let i = 0; i <16; i++) {
     childRow = document.createElement("div");
-    childRow.style.width = "20px";
+    childRow.style.width = "30px";
     childRow.style.height = "20px";
     childRow.style.border = "3px solid black"
     row.style.display = "flex";
     row.style.flexDirection = "row";
+    row.style.flexWrap = "wrap";
     //rowu konteynere şutla
     container.appendChild(row);
+    row.appendChild(childRow); 
     //childRow u row a şutla
-    row.appendChild(childRow);
+  }
+
+
 }
-}
-row_squares();
+
+
+
  //cells
-let cells = document.createElement("div");
-let childCells = document.createElement("div");
+
 //cells function
-function cells_squares(){
- //256 kere çalışacak bir div oluştur
- 
-  for(let i = 0; i<16; i++){
-    childCells = document.createElement("div");
-      
-    childCells.style.width = "20px";
-    childCells.style.height = "20px";
-    childCells.style.border = "3px solid black";
-    cells.style.display = "flex";
-    cells.style.flexDirection = "row";
-      cells.appendChild(childCells);
-      
-      row.appendChild(cells);
-      container.appendChild(cells);
-
-
-    }
+for(i = 0; i<16; i++){
+  row_squares();
 }
-cells_squares();
+
+
 
  // bir div oluştur
 
