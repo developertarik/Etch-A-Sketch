d39472b1189618f32e5eb64de5e4235b3ff91961
tarik <tarikozgur0607@gmail.com>
2022-07-23 20:49:33 +0300

    add more codes

--- script.js
+++ script.js
@@ -2,38 +2,52 @@
  //row
  //rowun özelliklerini ayarla
  let row = document.createElement("div");
- //container
+ let childRow = document.createElement("div");
  const container = document.querySelector("#container")
+ container.style.display = "flex";
+ container.style.flexDirection = "column";
+
+ //container
  
  function row_squares(){
     for (let i = 0; i <16; i++) {
-    let childRow = document.createElement("div")
     childRow = document.createElement("div");
-    childRow.style.width = "10px";
-    childRow.style.height = "10px";
-    childRow.style.border = "3px solid red"
-    childRow.style.display = "flex";
-    childRow.style.flexDirection = "row";
+    childRow.style.width = "20px";
+    childRow.style.height = "20px";
+    childRow.style.border = "3px solid black"
+    row.style.display = "flex";
+    row.style.flexDirection = "row";
+    //rowu konteynere şutla
     container.appendChild(row);
+    //childRow u row a şutla
     row.appendChild(childRow);
 }
-
- 
 }
 row_squares();
  //cells
-const cells = document.createElement("div");
+let cells = document.createElement("div");
+let childCells = document.createElement("div");
+//cells function
+function cells_squares(){
  //256 kere çalışacak bir div oluştur
+ 
   for(let i = 0; i<16; i++){
-      let div = document.createElement("div");
+    childCells = document.createElement("div");
       
-      div.style.width = "10px";
-      div.style.height = "10px";
-      div.style.border = "3px solid red";
+    childCells.style.width = "20px";
+    childCells.style.height = "20px";
+    childCells.style.border = "3px solid black";
+    cells.style.display = "flex";
+    cells.style.flexDirection = "row";
+      cells.appendChild(childCells);
       
-      document.getElementById("container").appendChild(div);
+      row.appendChild(cells);
+      container.appendChild(cells);
+
+
     }
-   
+}
+cells_squares();
 
  // bir div oluştur
 
