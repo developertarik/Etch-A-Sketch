1b5170382a41421a4a341b8dddb1095c51d891d7
tarik <tarikozgur0607@gmail.com>
2022-07-23 15:17:05 +0300

    is null problem solved

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+  // htmldeki divi oluştur
  
+  //256 kere çalışacak bir div oluştur
+  for(let i = 0; i<7; i++){
+      let div = document.createElement("div");
+      div.textContent = "oluştum amk";
+      document.body.appendChild(div);
+    }
+   
+
+ // bir div oluştur
+
+
+
+
