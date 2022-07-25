da5898cd2b4ca7be3434269b04b91989ef164561
tarik <tarikozgur0607@gmail.com>
2022-07-25 14:52:43 +0300

    change32x32

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
  let row = document.createElement("div");
  let childRow = document.createElement("div");
  const container = document.querySelector("#container")
-
+//button
+let btn = document.querySelector("button");
  container.style.display = "flex";
  container.style.flexDirection = "column";
  container.style.width = "600px";
@@ -24,7 +25,6 @@
     childRow.style.width = "30px";
     childRow.style.height = "20px";
     childRow.style.border = "3px solid black";
-    childRow.classList.add("square");
     row.style.display = "flex";
     row.style.flexDirection = "row";
     row.style.flexWrap = "wrap";
@@ -37,13 +37,20 @@
     childRow.addEventListener("mouseover",function(e){
       e.target.style.backgroundColor = "black";
     })
+    
   }
 
-
 }
+btn.addEventListener("click",open_32x32);
 
 
-
+function open_32x32(){
+  for (let i = 0; i<32; i++){
+  childRow = document.createElement("div");
+  childRow.style.width = "15px";
+  childRow.style.height = "10px";
+  childRow.style.border = "1.5px solid black";
+}}
 
  //cells
 
