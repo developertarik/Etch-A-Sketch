0ebcc903abf6df5b4ca5407d5b4aa78c344f61e5
tarik <tarikozgur0607@gmail.com>
2022-07-27 22:14:58 +0300

    solving problems

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
-  // htmldeki divi oluştur
- //row
- //rowun özelliklerini ayarla
- let row = document.createElement("div");
- let box = document.createElement("div");
- const container = document.querySelector("#container")
-//button
-let btn = document.querySelector("button");
- container.style.display = "flex";
- container.style.width = "500px";
- container.style.height = "500px";
- container.style.flexDirection = "column";
- container.style.flexWrap = "wrap";
- container.style.boxSizing = "border-box";
- container.style.marginLeft = "150px";
- row.style.display = "flex";
- //container
- let board = 16;
- 
- function gameZone(){
-  for(let j = 0; j<board; j++){
-    row = document.createElement("div");
-    for (let i = 0; i <board; i++) {
-    box = document.createElement("div");
-    row.classList.add("row");
-    box.classList.add("hover")    
-                                  
-    row.style.height = "%100";
-    box.style.width = "30px";
-    box.style.height = "20px";
-    box.style.border = "3px solid black";
-    row.style.display = "flex";
-    row.style.flexDirection = "row";
-    row.style.flexWrap = "wrap";
+    // htmldeki divi oluştur
+  //row
+  //rowun özelliklerini ayarla
+  let row = document.createElement("div");
+  let box = document.createElement("div");
+  const container = document.querySelector("#container");
 
-    //rowu konteynere şutla
-    container.appendChild(row);
-    row.appendChild(box); 
-    //childRow u row a şutla
-    
-    box.addEventListener("mouseover",function(e){
-      e.target.style.backgroundColor = "black";
-    })
+  //button
+  let btn = document.querySelector("button");
+  container.style.display = "flex";
+  container.style.flex ="1";
+    container.style.width = "500px";
+  container.style.height = "500px";
+  container.style.flexDirection = "column";
+  container.style.flexWrap = "wrap";
+
+  container.style.marginLeft = "150px";
+  row.style.display = "flex";
+  //container
+  let board = 32;
+  
+  function gameZone(){
+    for(let j = 0; j<board; j++){
+      row = document.createElement("div");
+      for (let i = 0; i <board; i++) {
+      box = document.createElement("div");
+      
+      row.style.height = "%100";
+      
+      box.style.width = 500 / board + "px";
+      box.style.height = 500 /board + "px";
+      box.style.border = "1px solid black";
+      row.style.display = "flex";
+      row.style.flexDirection = "row";
+      row.style.flexWrap = "wrap";
+      row.style.boxSizing = "borderBox";
+
+      //rowu konteynere şutla
+      container.appendChild(row);
+      row.appendChild(box); 
+      //childRow u row a şutla
+      
+      box.addEventListener("mouseover",function(e){
+        e.target.style.backgroundColor = "black";
+      })
+    };
+      
+    }
+  
+
+
   };
-    
-  }
- 
 
+  //cells
 
-};
-
- //cells
-
-//cells function
-    gameZone();
+  //cells function
+      gameZone();
 
 
 
- // bir div oluştur
+  // bir div oluştur
 
 
 
